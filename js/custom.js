92963797b8fde619d331ebb0e5912bcdf05d76fa
Pavan <pavanskipo@gmail.com>
2018-12-01 18:24:03 +0530

    Adding Team section.

--- js/custom.js
+++ js/custom.js
@@ -7,12 +7,24 @@ $(function () {
 
 // Work
 
-$(function() {
+$(function () {
     $("#work").magnificPopup({
         delegate: 'a',
         type: 'image',
         gallery: {
             enabled: true
         }
+    })
+});
+
+// Team
+
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
     })
 });
