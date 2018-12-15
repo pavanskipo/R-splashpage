96723f2ce120933c7149080b94e7e932cb9e86b0
Pavan <pavanskipo@gmail.com>
2018-12-15 22:28:01 +0530

    Adding navigation section.

--- js/custom.js
+++ js/custom.js
@@ -62,3 +62,28 @@ $(function () {
         autoplayHoverPause: true
     })
 });
+
+// Navigation
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 50) {
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+        } else {
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    })
+});
+
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});
