10ef5ad0033e93b81842be73d6d6005cf9f958c4
Pavan <pavanskipo@gmail.com>
2018-12-16 21:40:27 +0530

    Finalized the website along with SEO optimization.

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,18 @@ $(function () {
         autoplay: true,
         smartSpeed: 700,
         loop: true,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            }            
+        }
     })
 });
 
@@ -59,7 +70,21 @@ $(function () {
         autoplay: true,
         smartSpeed: 700,
         loop: true,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 3
+            },
+            768: {
+                items: 5
+            },
+            992: {
+                items: 6
+            }
+        }
     })
 });
 
@@ -87,3 +112,9 @@ $(function () {
         }, 1250, "easeInOutExpo");
     });
 });
+
+$(function () {
+    $(".navbar-collapse ul li a").on("click touch", function () {
+        $(".navbar-toggle").click();
+    })
+})
